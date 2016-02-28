165f51087aaa6e30fce787ed4858f632d0308ff3
Phyu Thae Aung <phyuthaeaung@phyus-macbook-air.local>
2016-02-28 12:50:42 +0630

    Initial Commit

---
+++ Red Page/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Red Page
+//
+//  Created by Phyu Thae Aung on 28/2/16.
+//  Copyright © 2016 Pai Innovation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
